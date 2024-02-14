7d6bda61327d6f4b2ec7a607406a0afdcee6cb2e
Arthur Wallendorff <howdy@wallenart.dev>
2024-02-15 00:25:09 +0100

    feat: better quickfix

---
+++ nvim/lua/plugins/utils/qf-helper.lua
@@ -0,0 +1,23 @@
+return {
+    "stevearc/qf_helper.nvim",
+    config = function()
+        local qf_helper = require("qf_helper")
+
+        qf_helper.setup({
+            prefer_loclist = false, -- Used for QNext/QPrev (see Commands below)
+            sort_lsp_diagnostics = true, -- Sort LSP diagnostic results
+            quickfix = {
+                autoclose = true, -- Autoclose qf if it's the only open window
+                default_bindings = false, -- Set up recommended bindings in qf window
+                default_options = true, -- Set recommended buffer and window options
+                max_height = 10, -- Max qf height when using open() or toggle()
+                min_height = 1, -- Min qf height when using open() or toggle()
+                track_location = true, -- Keep qf updated with your current location
+            },
+        })
+
+        vim.keymap.set("n", "<leader>qf", function()
+            vim.cmd("QFToggle!")
+        end, { desc = "Toggle [Q]uick[F]ix", silent = true })
+    end,
+}
