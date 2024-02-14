99e256296bc7a0a432dd25d20ef2179b99b66810
Arthur Wallendorff <howdy@wallenart.dev>
2024-02-15 00:21:42 +0100

    feat: add lspkind

--- nvim/lua/plugins/editor/nvim-cmp.lua
+++ nvim/lua/plugins/editor/nvim-cmp.lua
@@ -16,6 +16,9 @@ return {
         },
         "saadparwaiz1/cmp_luasnip",
 
+        -- Nice symbols in float
+        "onsails/lspkind.nvim",
+
         -- Adds LSP completion capabilities
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
@@ -27,12 +30,32 @@ return {
         -- See `:help cmp`
         local cmp = require("cmp")
         local luasnip = require("luasnip")
+        local lspkind = require("lspkind")
+
         require("luasnip.loaders.from_vscode").lazy_load()
-        luasnip.config.setup({})
+        luasnip.config.setup()
 
         local select_opts = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
+            formatting = {
+                fields = { "abbr", "kind", "menu" },
+                expandable_indicator = true,
+                format = lspkind.cmp_format({
+                    mode = "symbol_text", -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = "", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+                    -- The function below will be called before any actual modifications from lspkind
+                    -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                    before = function(_, vim_item)
+                        return vim_item
+                    end,
+                }),
+            },
             window = {
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
