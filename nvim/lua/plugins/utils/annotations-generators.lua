2d7ef3f821feb581a870c6e539d7034b9a1da988
Arthur Wallendorff <howdy@wallenart.dev>
2024-02-15 09:10:25 +0100

    chore: remove vim-doge

--- nvim/lua/plugins/utils/annotations-generators.lua
+++ nvim/lua/plugins/utils/annotations-generators.lua
@@ -1,53 +1,29 @@
 return {
     {
-        "kkoomen/vim-doge",
-        event = "BufRead",
-        build = ":call doge#install()",
-        config = function()
-            -- FIXME(vim-doge): this part does not work
-            -- Disabling default mappings
-            vim.g.doge_enable_mappings = 0
-            vim.g.doge_mapping = "<leader>g"
-        end,
-    },
-    {
         "danymat/neogen",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         version = "*", -- Uses only stable versions
         config = function()
-            local filetype = vim.bo.filetype
+            local neogen = require("neogen")
 
-            -- FIXME(generators): does not work w/ lua, not tested w/ js
-            local generators = {
-                {
-                    language = "lua",
-                    cmd = "Neogen",
+            neogen.setup({
+                enabled = true,
+                languages = {
+                    lua = {
+                        snippet_engine = "luasnip",
+                    },
                 },
-                {
-                    language = "javascript",
-                    cmd = "call doge#generate()",
-                },
-            }
+            })
 
-            local has_generated = false
-            local function generate_annotations()
-                for _, generator in ipairs(generators) do
-                    if filetype == generator.language then
-                        vim.cmd(generator.cmd)
-                        has_generated = true
-                    end
-                end
+            vim.keymap.set(
+                "n",
+                "<leader>g",
+                neogen.generate,
+                { desc = "[G]enerate type annotations (JS/Lua)", silent = true }
+            )
 
-                return has_generated
-            end
-
-            vim.keymap.set("n", "<leader>g", function()
-                generate_annotations()
-
-                if has_generated == false then
-                    vim.notify("No generator for filetype " .. filetype, vim.log.levels.INFO)
-                end
-            end, { desc = "[G]enerate type annotations (JS/Lua)", silent = true })
+            vim.keymap.set({ "n", "i", "v" }, "<C-l>", neogen.jump_next)
+            vim.keymap.set({ "n", "i", "v" }, "<C-h>", neogen.jump_prev)
         end,
     },
 }
