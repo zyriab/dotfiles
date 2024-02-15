6e0a4f60aab5a201a9e7ba9d84212335ecd6bd8b
Arthur Wallendorff <howdy@wallenart.dev>
2024-02-16 00:41:29 +0100

    doc: tweak registered keymaps

--- nvim/lua/configs/lsp/register-which-keys.lua
+++ nvim/lua/configs/lsp/register-which-keys.lua
@@ -3,10 +3,10 @@ return function()
     require("which-key").register({
         ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
         ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-        ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+        ["<leader>g"] = { name = "[G]olang", _ = "which_key_ignore" },
         ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
         ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+        ["<leader>s"] = { name = "[S]earch (Telescope)", _ = "which_key_ignore" },
         ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
         ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
     })
