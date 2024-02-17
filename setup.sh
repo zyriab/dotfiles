b052957b412b2899f930807ea7afed240ee5b914
Arthur Wallendorff <howdy@wallenart.dev>
2024-02-17 23:22:21 +0100

    chore: refactor + add bat

--- setup.sh
+++ setup.sh
@@ -6,13 +6,15 @@ if [ "$EUID" -ne 0 ]
 fi
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+CONFIG_DIR="$USER_HOME/.config"
 
-ln -sfn "$PWD/zsh/.zshrc" $USER_HOME/
-ln -sfn "$PWD/zsh/.bash_aliases" $USER_HOME/
+ln -sfn "$PWD/zsh/.zshrc" "$USER_HOME"
+ln -sfn "$PWD/zsh/.bash_aliases" "$USER_HOME"
 
-ln -sfn "$PWD/nvim" $USER_HOME/.config/
-ln -sfn "$PWD/wezterm" $USER_HOME/.config/wezterm
-ln -sfn "$PWD/wezterm/.wezterm.lua" $USER_HOME/.wezterm.lua
-ln -sfn "$PWD/lazygit" $USER_HOME/.config/
+ln -sfn "$PWD/nvim" "$CONFIG_DIR"
+ln -sfn "$PWD/wezterm" "$CONFIG_DIR"
+ln -sfn "$PWD/wezterm/.wezterm.lua" "$USER_HOME"
+ln -sfn "$PWD/lazygit" "$CONFIG_DIR"
+ln -sfn "$PWD/bat" "$CONFIG_DIR"
 
-ln -sfn "$PWD/redshift/redshift.conf" $USER_HOME/.config/
+ln -sfn "$PWD/redshift/redshift.conf" "$CONFIG_DIR"
