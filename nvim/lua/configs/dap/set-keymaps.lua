b97702f94c1c7b60b11acc1598de04e96b8dcd39
Arthur Wallendorff <howdy@wallenart.dev>
2024-02-21 23:03:05 +0100

    feat: add persistant breakpoints

--- nvim/lua/configs/dap/set-keymaps.lua
+++ nvim/lua/configs/dap/set-keymaps.lua
@@ -1,4 +1,5 @@
 local dap = require("dap")
+local persistent_breakpoints = require("persistent-breakpoints.api")
 
 return function()
     -- HACK(DAP): Until I write my own Go config,
@@ -19,9 +20,25 @@ return function()
     vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
     vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
     vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle [B]reakpoint" })
-    vim.keymap.set("n", "<leader>B", function()
-        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-    end, { desc = "Debug: Set [B]reakpoint" })
-    vim.keymap.set("n", "<leader>cb", dap.clear_breakpoints, { desc = "Debug: [C]lear [B]reakpoints" })
+
+    vim.keymap.set("n", "<leader>b", persistent_breakpoints.toggle_breakpoint, { desc = "Debug: Toggle [B]reakpoint" })
+    vim.keymap.set(
+        "n",
+        "<leader>B",
+        persistent_breakpoints.set_conditional_breakpoint,
+        { desc = "Debug: Set conditional [B]reakpoint" }
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>cb",
+        persistent_breakpoints.clear_all_breakpoints,
+        { desc = "Debug: [C]lear [B]reakpoints" }
+    )
+
+    -- NOTE: replaced by `persistent_breakpoints` plugin
+    -- vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle [B]reakpoint" })
+    -- vim.keymap.set("n", "<leader>B", function()
+    --     dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+    -- end, { desc = "Debug: Set conditional [B]reakpoint" })
+    -- vim.keymap.set("n", "<leader>cb", dap.clear_breakpoints, { desc = "Debug: [C]lear [B]reakpoints" })
 end
