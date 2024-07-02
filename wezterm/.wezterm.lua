17a192d7c1d12ebb69a4ae2e8292c7285098aabc
Arthur Wallendorff <howdy@wallenart.dev>
2024-07-02 11:22:47 +0200

    feat: better multiplexing + sessions

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,11 +1,14 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 local act = wezterm.action
-local mux = wezterm.mux
 
 local function is_linux()
 	return io.popen("uname -s"):read() == "Linux"
 end
+
+wezterm.on("update-right-status", function(window, _)
+	window:set_right_status(window:active_workspace())
+end)
 
 -- This table will hold the configuration.
 local config = {}
@@ -36,8 +39,12 @@ config.use_fancy_tab_bar = false
 config.mouse_wheel_scrolls_tabs = false
 config.show_new_tab_button_in_tab_bar = false
 
--- Keymaps
+-- Cursor
+config.default_cursor_style = "SteadyBar"
 
+local pane_size_step = 5
+
+-- Keymaps
 config.leader = { key = " ", mods = "ALT" }
 config.keys = {
 	{ key = "n", mods = "SHIFT|CTRL", action = act.ToggleFullScreen },
@@ -46,6 +53,14 @@ config.keys = {
 	{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
 	{ key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- Pane resizing/zooming/moving
+	{ key = "<", mods = "LEADER", action = act.AdjustPaneSize({ "Left", pane_size_step }) },
+	{ key = ">", mods = "LEADER", action = act.AdjustPaneSize({ "Right", pane_size_step }) },
+	{ key = "+", mods = "LEADER", action = act.AdjustPaneSize({ "Up", pane_size_step }) },
+	{ key = "-", mods = "LEADER", action = act.AdjustPaneSize({ "Down", pane_size_step }) },
+	{ key = "x", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	{ key = "m", mods = "LEADER", action = act.TogglePaneZoomState },
 
 	-- Move between panes
 	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
@@ -61,5 +76,11 @@ config.keys = {
 	{ key = "+", mods = "SHIFT|CTRL", action = act.DisableDefaultAssignment },
 	{ key = "=", mods = "CTRL", action = act.DisableDefaultAssignment },
 	{ key = "_", mods = "CTRL", action = act.DisableDefaultAssignment },
+
+	-- WezMux
+	{ key = "9", mods = "ALT", action = act({ ShowLauncherArgs = { flags = "FUZZY|WORKSPACES" } }) },
+	{ key = "n", mods = "ALT", action = act.SwitchWorkspaceRelative(1) },
+	{ key = "p", mods = "ALT", action = act.SwitchWorkspaceRelative(-1) },
 }
+
 return config
