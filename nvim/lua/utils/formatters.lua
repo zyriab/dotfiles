1221d5e52c711f59bcc6b085da6627fdb46a13c2
Arthur Wallendorff <howdy@wallenart.dev>
2024-07-24 22:41:28 +0200

    fix: properly format last line + do not format before frontmatter (unused)

--- nvim/lua/utils/formatters.lua
+++ nvim/lua/utils/formatters.lua
@@ -5,32 +5,27 @@ local M = {}
 M.lsp_format_skip_frontmatter = function()
     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     local in_frontmatter = false
-    local start_line, end_line
+    local frontmatter_start_line, frontmatter_end_line
 
     for i, line in ipairs(lines) do
         if line:match("^%-%-%-[%w]*$") then
             if not in_frontmatter then
                 in_frontmatter = true
-                start_line = i
+                frontmatter_start_line = i
             else
-                end_line = i
+                frontmatter_end_line = i
                 break
             end
         end
     end
 
-    if start_line and end_line then
+    if frontmatter_start_line and frontmatter_end_line then
         -- Format everything except the frontmatter
+
         vim.lsp.buf.format({
             range = {
-                ["start"] = { 1, 0 },
-                ["end"] = { start_line - 1, 0 },
-            },
-        })
-        vim.lsp.buf.format({
-            range = {
-                ["start"] = { end_line + 1, 0 },
-                ["end"] = { #lines, 0 },
+                ["start"] = { frontmatter_end_line + 1, 0 },
+                ["end"] = { #lines + 1, 0 },
             },
         })
     else
